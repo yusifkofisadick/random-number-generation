82813fa404ec59f7607b573f9b2a223dcf519886
yusif kofi sadick <124945382+yusifkofisadick@users.noreply.github.com>
2026-09-20 15:31:36 -0700

    Append percentage sign to love score alert

--- loveScore.js
+++ loveScore.js
@@ -3,4 +3,4 @@ prompt("What is your name?");
 var liveScore = Math.random() * 100;
 liveScore = Math.floor(liveScore) + 1;
 
-alert("Your love score is: " + liveScore);
+alert("Your love score is: " + liveScore + "%");
